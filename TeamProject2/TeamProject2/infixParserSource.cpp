d36e94ded88b097caa9b5d06f00445e55754a435
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-04-10 04:47:55 -0500

    Update infixParserSource.cpp
    Handled input and output streams.

--- TeamProject2/TeamProject2/infixParserSource.cpp
+++ TeamProject2/TeamProject2/infixParserSource.cpp
@@ -6,19 +6,22 @@
 using namespace std;
 
 int main() {
-    ifstream fin("testCaseFile.txt");
+   ifstream fin("testCaseFile.txt");
 
-    if (!fin.is_open()) {
-        cout << "Could not open file" << endl;
-    }
-    else {
+   if (!fin.is_open()) {
+       cout << "Could not open file" << endl;
+       return -1;
+   }
+   else {
         ofstream fout;
         string line;
         Expression expressionVec = Expression();
         while (getline(fin, line)) { //Gets each line from the input file
-            expressionVec.pushBack(line);
+            cout << "Original expression: " << line << endl;
+            Expression e;
+            cout << "Postfix expression: " << e.toPostfix(line) << endl;
+            cout << "Evaluated expression: " << e.evaluate(e.toPostfix(line)) << endl;
         }
-
-        expressionVec.print();
-    }
+   }
+   return 0;
 }
