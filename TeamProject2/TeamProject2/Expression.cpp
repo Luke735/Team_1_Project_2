191d439dcc76a940292ac07925b676269cfa3805
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-04-10 04:54:14 -0500

    Update Expression.cpp
    implemented the toPostFix function.

--- TeamProject2/TeamProject2/Expression.cpp
+++ TeamProject2/TeamProject2/Expression.cpp
@@ -1,5 +1,11 @@
 #include "Expression.h"
 
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
 using namespace std;
 
 //Constructor, does nothing
@@ -10,21 +16,99 @@ int Expression::precedence(string str) {
 	if (str == "*" || str == "/" || str == "%") { return 2; }
 	if (str == "+" || str == "-") { return 3; }
 	if (str == ">" || str == ">=" || str == "<" || str == "<=") { return 4; }
-	if (str == "=" || str == "!") { return 5; }
-	if (str == "&") { return 6; }
-	if (str == "|") { return 7; }
+	if (str == "==" || str == "!=") { return 5; }
+	if (str == "&&") { return 6; }
+	if (str == "||") { return 7; }
+	return 0;
 }
 
 /** Returns the postfix expression of the infix string passed to it.
-	@param infixExp: the infix expression
+	@param exp: the infix expression
 	@return: the postfix expression
 */
-string Expression::toPostfix(int index) {
-	return infixExpressions.at(index);
+string Expression::toPostfix(string exp) {
+	exp.erase(remove(exp.begin(), exp.end(), ' '), exp.end());
+	stack<string> s;
+	unordered_map<string, int> precedenceMap;
+	string postfixExp = "";
+	string token;
+
+	//Populate the precedence map
+	for (int i = 0; i < exp.length(); i++) {
+		token = exp.substr(i, 2);
+		if (precedence(token) != 0) {
+			precedenceMap[token] = precedence(token);
+			i++;
+		}
+		else {
+			token = exp.substr(i, 1);
+			if (precedence(token) != 0) {
+				precedenceMap[token] = precedence(token);
+			}
+		}
+	}
+
+	//Process each token in the map
+	for (int i = 0; i < exp.length(); i++) {
+		token = exp.substr(i, 2);
+		if (precedence(token) != 0) {
+			//Handles all boolean operators
+			while (!s.empty() && s.top() != "(" && precedenceMap[s.top()] <= precedence(token)) {
+				postfixExp += s.top() + " ";
+				s.pop();
+			}
+			s.push(token);
+			i++;
+		}
+		else {
+			token = exp.substr(i, 1);
+			if (precedence(token) != 0) {
+				//Handles all other operators
+				while (!s.empty() && s.top() != "(" && precedenceMap[s.top()] <= precedence(token)) {
+					postfixExp += s.top() + " ";
+					s.pop();
+				}
+				s.push(token);
+			}
+			else if (token == "(") {
+				s.push(token);
+			}
+			else if (token == ")") {
+				//Handles operators inside of parentheses
+				while (!s.empty() && s.top() != "(") {
+					postfixExp += s.top() + " ";
+					s.pop();
+				}
+				if (!s.empty() && s.top() == "(") {
+					s.pop();
+				}
+			}
+			else {
+				//Processes operands
+				postfixExp += token;
+				if (i < exp.length() - 1) {
+					if (!isdigit(exp.at(i + 1))) { postfixExp += ' '; }
+				}
+				else{ postfixExp += ' '; }
+			}
+		}
+	}
+
+	//Pop all remaining operators
+	while (!s.empty()) {
+		postfixExp += s.top() + " ";
+		s.pop();
+	}
+
+	postfixExp.pop_back();
+
+	//Return the postfix expression
+	return postfixExp;
+
 }
 
 void Expression::pushBack(string str) {
-	infixExpressions.push_back(str);
+
 }
 
 int Expression::evaluate(string str) {
@@ -34,8 +118,8 @@ int Expression::evaluate(string str) {
 void Expression::print() {
 	for (size_t i = 0; i < infixExpressions.size(); ++i) {
 		cout << "INFIX: " << endl << infixExpressions.at(i) << endl;
-		string postfixStr = this->toPostfix(i);
-		cout << "POSTFIX: " << endl << postfixStr << endl;
-		cout << "EVALUATED: " << endl << evaluate(postfixStr) << endl;
+		//string postfixStr = this->toPostfix(i);
+		//cout << "POSTFIX: " << endl << postfixStr << endl;
+		//cout << "EVALUATED: " << endl << evaluate(postfixStr) << endl;
 	}
 }
